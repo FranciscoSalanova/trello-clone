d7e4ddcbf4a98f79f5cfbfb0d5cf1c5cdf911ca5
FranciscoSalanova <fsalanova.16@gmail.com>
2024-06-22 14:17:57 -0300

    Completing item clone for dragging element

--- src/dragAndDrop.js
+++ src/dragAndDrop.js
@@ -1,33 +1,56 @@
-import addGlobalEventListener from "../utils/addGlobalEventListener.js"
+import addGlobalEventListener from '../utils/addGlobalEventListener.js'
 
 export default function setupDragAndDrop() {
-  addGlobalEventListener("mousedown", "[data-draggable]", (e) => {
+  addGlobalEventListener('mousedown', '[data-draggable]', (e) => {
     const selectedNode = e.target
     const itemClone = selectedNode.cloneNode(true)
-    itemClone.classList.add("dragging")
+    const ghost = selectedNode.cloneNode()
 
-    positionClone(itemClone, e)
-    document.body.append(itemClone)
-    selectedNode.classList.add("hide")
+    const offset = setupDragItems(selectedNode, itemClone, ghost, e)
 
-    const moveMouseFunction = (e) => {
-      positionClone(itemClone, e)
-    }
-    document.addEventListener("mousemove", moveMouseFunction)
-
-    document.addEventListener(
-      "mouseup",
-      (e) => {
-        document.removeEventListener("mousemove", moveMouseFunction)
-        selectedNode.classList.remove("hide")
-        itemClone.remove()
-      },
-      { once: true }
-    )
+    setupDragEvents(selectedNode, itemClone, offset)
   })
 }
 
-function positionClone(itemClone, mousePosition) {
-  itemClone.style.top = `${mousePosition.clientY}px`
-  itemClone.style.left = `${mousePosition.clientX}px`
+function setupDragItems(selectedNode, itemClone, ghost, e) {
+  const originalRect = selectedNode.getBoundingClientRect()
+  const offset = {
+    x: e.clientX - originalRect.left,
+    y: e.clientY - originalRect.top,
+  }
+
+  selectedNode.classList.add('hide')
+
+  itemClone.classList.add('dragging')
+  itemClone.style.width = `${originalRect.width}px`
+  positionClone(itemClone, e, offset)
+  document.body.append(itemClone)
+
+  ghost.classList.add('ghost')
+  ghost.innerHTML = ''
+  selectedNode.parentElement.insertBefore(ghost, selectedNode)
+
+  return offset
+}
+
+function setupDragEvents(selectedNode, itemClone, offset) {
+  const moveMouseFunction = (e) => {
+    positionClone(itemClone, e, offset)
+  }
+  document.addEventListener('mousemove', moveMouseFunction)
+
+  document.addEventListener(
+    'mouseup',
+    (e) => {
+      document.removeEventListener('mousemove', moveMouseFunction)
+      selectedNode.classList.remove('hide')
+      itemClone.remove()
+    },
+    { once: true }
+  )
+}
+
+function positionClone(itemClone, mousePosition, offset) {
+  itemClone.style.top = `${mousePosition.clientY - offset.y}px`
+  itemClone.style.left = `${mousePosition.clientX - offset.x}px`
 }
